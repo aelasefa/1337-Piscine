66823bcabfe771ea664623fb0a649c6835620273
aelasefa <aymanelasefar1337@gmail.com>
2025-04-19 19:36:19 +0100

    C00

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayelasef <ayelasef@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 10:11:11 by ayelasef          #+#    #+#             */
+/*   Updated: 2024/06/27 10:38:05 by ayelasef         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_char(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+}
+
+void	ft_print_comb(void)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_print_char(a, b, c);
+				if (a != '7')
+				{
+					write(1, ", ", 2);
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
