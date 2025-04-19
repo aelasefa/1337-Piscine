dde4683bd64e5aef782687575dda4e97b7349476
aelasefa <aymanelasefar1337@gmail.com>
2025-04-19 19:43:44 +0100

    C05

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayelasef <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 21:55:04 by ayelasef          #+#    #+#             */
+/*   Updated: 2024/07/08 21:59:18 by ayelasef         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb <= 0 || nb == 1)
+		return (0);
+	if (nb == 2)
+		return (1);
+	i = 2;
+	while (i <= nb / 2)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (ft_is_prime(nb))
+		return (nb);
+	return (ft_find_next_prime(nb + 1));
+}
