52e2efb2259f462ef96d1496c5c6dc50f8f38327
aelasefa <aymanelasefar1337@gmail.com>
2025-04-19 19:43:15 +0100

    C03

---
+++ C03/ex05/ft_strlcat.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ayelasef <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/02 20:55:44 by ayelasef          #+#    #+#             */
+/*   Updated: 2024/07/03 20:27:38 by ayelasef         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+unsigned int	ft_strlen(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	destlen;
+
+	j = 0;
+	while (dest[j])
+		j++;
+	if (j >= size)
+	{
+		return (size + ft_strlen(src));
+	}
+	destlen = j;
+	i = 0;
+	while (src[i] && i < size - destlen - 1)
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
+	return (ft_strlen(src) + destlen);
+}
